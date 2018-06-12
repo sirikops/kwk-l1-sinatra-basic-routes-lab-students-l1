84e38fd27d2ee6934809cdfe0e6792c549080088
sirikops <sirikops@hotmail.com>
2018-06-12 21:50:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Siri."
+  end
+  get '/hometown' do
+    "My hometown is San Jose."
+  end
+  get '/favorite-song' do
+    "My favorite song is Bohemian Rhapsody by Queen."
+  end
 end
